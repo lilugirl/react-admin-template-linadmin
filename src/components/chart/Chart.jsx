b6def4819ebe03baa4b97bbaf2493e0a651adeda
liu yi <lilugirl2005@126.com>
2022-05-12 02:04:03 +0800

    chart

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,75 +1,72 @@
-import './chart.scss'
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import "./chart.scss";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
+  {
+    name: "January",
+    Total: 1200,
+  },
+  {
+    name: "Februray",
+    Total: 2100,
+  },
+  {
+    name: "March",
+    Total: 800,
+  },
+  {
+    name: "April",
+    Total: 1600,
+  },
+  {
+    name: "May",
+    Total: 900,
+  },
+  {
+    name: "June",
+    Total: 1700,
+  },
+];
 
 const Chart = () => {
   return (
-    <div className='chart'>
-         <ResponsiveContainer width="100%" height="100%">
+    <div className="chart">
+      <div className="title">Last 6 Months (Revenue)</div>
+      <ResponsiveContainer width="100%" height="100%">
         <AreaChart
-          width={500}
-          height={400}
+          width={730}
+          height={250}
           data={data}
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
+          <defs>
+            <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" stroke="gray" />
+          <CartesianGrid strokeDasharray="3 3" className="chartGrid" />
           <Tooltip />
-          <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+          <Area
+            type="monotone"
+            dataKey="Total"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#total)"
+          />
         </AreaChart>
       </ResponsiveContainer>
     </div>
-  )
-}
+  );
+};
 
-export default Chart
+export default Chart;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import './home.scss'
 import Widget from '../../components/widget/Widget'
 import Featured from '../../components/featured/Featured'
 import Chart from '../../components/chart/Chart'
+import Table from '../../components/table/Table'
 
 const Home=()=> {
   return (
@@ -20,6 +21,12 @@ const Home=()=> {
            <div className="charts">
             <Featured />
             <Chart/>
+           </div>
+           <div className="listContainer">
+             <div className="listTitle">
+                Latest Transactions
+             </div>
+             <Table />
            </div>
         </div>
     </div>
